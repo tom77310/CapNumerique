83ecabece9fb0540c6677fbf2e72c05dcf4960c4
Thomas Lemaire <lemairetomtom@gmail.com>
2026-04-07 12:44:07 +0200

    ajout des pages du footer, mentions legales, politique de confidentialite, et CGU

---
+++ src/Controller/FooterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/footer')]
+final class FooterController extends AbstractController
+{
+    // Page de mentions légales
+    #[Route('/mentionslegales', name: 'footer_mentionslegales')]
+    public function MentionsLegales(): Response
+    {
+        return $this->render('footer/mentionslegales.html.twig');
+    }
+    
+    // Page Politique de confidentialité
+    #[Route('/politiqueconf', name: 'footer_confidentialite')]
+    public function PolitiqueConf(): Response
+    {
+        return $this->render('footer/confidentialite.html.twig');
+    }
+
+    // Page CGU
+    #[Route('/cgu', name: 'footer_cgu')]
+    public function CGU(): Response
+    {
+        return $this->render('footer/CGU.html.twig');
+    }
+}
